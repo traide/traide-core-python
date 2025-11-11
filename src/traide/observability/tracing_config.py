838aaa1029a0bf1499d74a8d4c350bba3fee8476
Stefan Lengton <stefan@lengton.life>
2025-11-11 16:17:32 +0100

    fix: suppress BoundedDict deprecation warning
    Suppress deprecation warning from CloudTraceSpanExporter.
    Refs: https://github.com/GoogleCloudPlatform/opentelemetry-operations-python/issues/226

--- src/traide/observability/tracing_config.py
+++ src/traide/observability/tracing_config.py
@@ -1,14 +1,29 @@
+import warnings
 from enum import StrEnum
+from typing import Any
 
 from opentelemetry import metrics, trace
-from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter  # type: ignore
+from opentelemetry.exporter import cloud_trace
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.exporter.prometheus import PrometheusMetricReader
 from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor  # type: ignore
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.resources import SERVICE_INSTANCE_ID, SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, SpanExportResult
+
+
+class CloudTraceSpanExporterWithoutWarnings(cloud_trace.CloudTraceSpanExporter):
+    """Suppresses DeprecationWarning raised by CloudTraceSpanExporter.
+
+    See for details:
+    https://github.com/GoogleCloudPlatform/opentelemetry-operations-python/issues/226
+    """
+
+    def export(self, *args: Any, **kwargs: Any) -> SpanExportResult:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=DeprecationWarning)
+            return super().export(*args, **kwargs)
 
 
 class TracingType(StrEnum):
@@ -29,7 +44,7 @@ def configure_tracing(service_name: str, hostname: str, tracing_type: TracingTyp
     if tracing_type == TracingType.CONSOLE:
         traceProvider.add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
     else:
-        google_trace_exporter = BatchSpanProcessor(CloudTraceSpanExporter())  # type: ignore
+        google_trace_exporter = BatchSpanProcessor(CloudTraceSpanExporterWithoutWarnings())  # type: ignore
         traceProvider.add_span_processor(google_trace_exporter)
     trace.set_tracer_provider(traceProvider)
 
